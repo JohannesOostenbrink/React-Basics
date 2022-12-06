349690f7fb7ae4fa9a6d28158f575b066848627f
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-06 14:46:01 +0700

    Practice passing props between components

---
+++ passing_props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Any other name than Bob"/>
+            <Heading firstName = "Jack"/>
+        </div>
+    );
+};
+
+export default App;
