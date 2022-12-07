4788d0ff6ce647b999011e082d0b1151c7fd3a2b
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-07 14:05:53 +0700

    Dynamic Events

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,15 +1,35 @@
-import Btn from './Btn';
-import './App.css';
-import ModeToggler from './ModeToggler';
+// import Btn from './Btn';
+//import './App.css';
+// import ModeToggler from './ModeToggler';
 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Btn />
+//       <ModeToggler />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <Btn />
-     <ModeToggler />
-    </div>
-  );
+  function handleClick() { 
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number'); 
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+  return(
+    <button onClick = {handleClick}>
+      Guess the number between 1 and 3. 
+    </button>
+  )
 }
 
-export default App;
+  export default App;
--- practice/src/ModeToggler.js
+++ practice/src/ModeToggler.js
@@ -1,25 +1,26 @@
- function ModeToggler() {
-    const DarkModeOn = false;
-    const DarkMode = <h1>Dark Mode is on<h1>
-    const LightMode = <h1>Light Mode is on</h1>
+//  function ModeToggler() {
+//     const DarkModeOn = false;
+//     const DarkMode = <h1>Dark Mode is on<h1>
+//     const LightMode = <h1>Light Mode is on</h1>
 
-    handleClick () => {
-        DarkModeOn = !DarkModeOn;
-        if(DarkModeOn == True){
-            console.log(Dark Mode is on)
-        } else{
-            console.log(Light Mode is on)
-        }
-        }
+//   function handleClick() {
+//         DarkModeOn = !DarkModeOn
+//         if(DarkModeOn == True){
+//             console.log(Dark Mode is on)
+//         } else{
+//             console.log(Light Mode is on)
+//         }
+//         }
 
-    return(
-        <div>
-            {DarkModeOn? DarkMode : LightMode}
-            <button onClick = {handleClick}>
-                Click Me
-            </button>
-        </div>
-    )
-    }
+//     return(
+//         <div>
+//             {DarkModeOn? DarkMode : LightMode}
+//             <button onClick = {handleClick}>
+//                 Click Me
+//             </button>
+//         </div>
+//     )
+//     }
 
-export default ModeToggler
+// export default ModeToggler
+
